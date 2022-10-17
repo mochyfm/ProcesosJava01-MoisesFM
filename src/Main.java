a1dbbe5eb96bcabac2a50640cfb4b60d16629c72
Moisés Fernández Muiña (Home) <mochyfm@gmail.com>
2022-10-17 18:26:48 +0100

    Pending to Finnish

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
-        ProcessUtils.callProcessNumber(3);
+        // ProcessUtils.callProcessNumber(3);
+        ProcessUtils.callTemporaryProcessNumber(2, 500);
     }
 }
