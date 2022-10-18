b9d25a19137a0d7be6307a08aca9d6031158bf7a
Moisés Fernández Muiña <mochyfm@gmail.com>
2022-10-18 13:08:14 +0100

    "Feature(ProcessUtils.java): Added new method "pipeLineInstruction":
    Created the new method cor getting the number of lines from a file.

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        // ProcessUtils.callProcessNumber(3);
-        ProcessUtils.callTemporaryProcessNumber(2, 500);
+        Scanner lectura = new Scanner(System.in);
+
+        System.out.print("¿Cuantos notepads quieres abrir?: ");
+        // ProcessUtils.callProcessNumber(lectura.nextInt();
+        lectura.nextLine();
+        System.out.print("¿Cuantos notepads quieres abrir? serán cerrados al pasar 500ms desde que fueron creados: ");
+        // ProcessUtils.callTemporaryProcessNumber(lectura.nextInt(), 500);
+        lectura.nextLine();
+
+        ProcessUtils.pipeLineInstruction(new File("./ProcesosJava01-MoisesFM.iml"));
+
+        // Ejemplo que falla.
+        // ProcessUtils.pipeLineInstruction(new File(".gitIgnora"));
     }
 }
